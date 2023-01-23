5c6e30ca2c8da16e29ac6a21c2e01f3a216e5f77
AsherEarly <asher.early@duke.edu>
2023-01-23 15:48:16 -0500

    just deleted the commented out Discord ENV lines that are no longer relevant

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -33,8 +33,6 @@ export const serverEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 };
 
 /**

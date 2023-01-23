634cc686a285816d34e8e173ce4f09827efc4a6f
AsherEarly <asher.early@duke.edu>
2023-01-23 17:14:04 -0500

    added redirect from sign in to /dashboard page

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -53,7 +53,7 @@ export const authOptions: NextAuthOptions = {
         }
       }
     })
-  ],
+  ]
 };
 
 export default NextAuth(authOptions);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ const AuthShowcase: React.FC = () => {
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
+        onClick={sessionData ? () => void signOut() : () => void signIn("credentials",{ callbackUrl: 'http://localhost:3000/dashboard' })}
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>

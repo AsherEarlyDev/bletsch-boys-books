005935eac519d27af41f5a2a93ad345a074089c8
Jeremy Cross <remycross1019@gmail.com>
2023-01-23 15:56:52 -0500

    Added secret to nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,6 +20,7 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
+  secret: process.env.NEXTAUTH_SECRET,
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [

a7f32251bdfe74f7904450e742c1a683f0334bb8
AsherEarly <asher.early@duke.edu>
2023-01-25 17:03:06 -0500

    added basic frontend table for inv page

--- src/pages/records.tsx
+++ src/pages/records.tsx
@@ -1,10 +1,12 @@
 import AppShell from "../components/AppShell";
+import Table from "../components/Table";
 
 
 export default function RecordPage() {
   return (
       <>
         <AppShell activePage="Records"></AppShell>
+        <Table></Table>
       </>
   )
 }

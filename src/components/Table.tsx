4c35e9b9814da671ddd901911921550743d291b4
AsherEarly <asher.early@duke.edu>
2023-01-29 16:24:36 -0500

    progress on table and modal React components

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,7 @@
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
-const people = [
-  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+const book = [
+  { name: 'Book 1', isbn: '13478392489', author: 'John Snow', price: 100, genre: 'comedy', inventory: 5},
   // More people...
 ]
 
@@ -24,11 +24,11 @@ export default function Table() {
             </button>
           </div>
         </div>
-        <div className="mt-8 flex flex-col">
+        <div className=" mt-8 flex flex-col">
           <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-300">
+                <table className="min-w-full divide-y divide-gray-300 table-auto">
                   <thead className="bg-gray-50">
                   <tr>
                     <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
@@ -42,14 +42,6 @@ export default function Table() {
                     <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                       <a href="#" className="group inline-flex">
                         ISBN
-                        <span className="ml-2 flex-none rounded bg-gray-200 text-gray-900 group-hover:bg-gray-300">
-                          <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
-                        </span>
-                      </a>
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      <a href="#" className="group inline-flex">
-                        Email
                         <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
                           <ChevronDownIcon
                               className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
@@ -60,7 +52,7 @@ export default function Table() {
                     </th>
                     <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                       <a href="#" className="group inline-flex">
-                        Role
+                        Author(s)
                         <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
                           <ChevronDownIcon
                               className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
@@ -69,23 +61,58 @@ export default function Table() {
                         </span>
                       </a>
                     </th>
-                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      <a href="#" className="group inline-flex">
+                        Price
+                        <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+                          <ChevronDownIcon
+                              className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
+                              aria-hidden="true"
+                          />
+                        </span>
+                      </a>
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      <a href="#" className="group inline-flex">
+                        Genre
+                        <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+                          <ChevronDownIcon
+                              className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
+                              aria-hidden="true"
+                          />
+                        </span>
+                      </a>
+                    </th>
+                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      <a href="#" className="group inline-flex">
+                        Inventory
+                        <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+                          <ChevronDownIcon
+                              className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
+                              aria-hidden="true"
+                          />
+                        </span>
+                      </a>
+                    </th>
+                    <th scope="col" className="relative py-3.5 pl-3 pr-0 sm:pr-6">
                       <span className="sr-only">Edit</span>
                     </th>
                   </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200 bg-white">
-                  {people.map((person) => (
-                      <tr key={person.email}>
+                  {book.map((book) => (
+                      <tr>
                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                          {person.name}
+                          {book.name}
                         </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.title}</td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.email}</td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.role}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{book.isbn}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{book.author}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{book.price}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{book.genre}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{book.inventory}</td>
                         <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                           <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                            Edit<span className="sr-only">, {person.name}</span>
+                            Edit<span className="sr-only">, {book.name}</span>
                           </a>
                         </td>
                       </tr>

f6ec3f0a2a031b21055539d64d6553ace1059fdf
Jeremy Cross <remycross1019@gmail.com>
2023-01-31 20:24:12 -0500

    Finalized login and page authorization

---
+++ src/pages/unauthorized.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+
+
+const Unauthorized: NextPage = (): JSX.Element => {
+    return(
+        <p>This page is unauthorized</p>
+    )
+};
+
+export default Unauthorized;

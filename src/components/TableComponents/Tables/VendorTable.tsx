d608cfcde925a278ea7986ea3f2c6d8940128e17
AsherEarly <asher.early@duke.edu>
2023-02-20 18:07:08 -0500

    Fixed small error, pulling to main

--- src/components/TableComponents/Tables/VendorTable.tsx
+++ src/components/TableComponents/Tables/VendorTable.tsx
@@ -10,128 +10,149 @@ import EditVendorModal from "../Modals/VendorModals/EditVendorModal";
 import ViewVendorModal from "../Modals/VendorModals/ViewVendorModal";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import SortedFilterableColumnHeading from '../TableColumnHeadings/SortedFilterableColumnHeading';
 import Table from "./Table";
 
 
-
 export default function VendorTable() {
-    const VENDORS_PER_PAGE = 5
-    const FIRST_HEADER =  ["Vendor Name", "name"]
-    const SORTABLE_HEADERS = []
-    const STATIC_HEADERS = ["Edit", "Delete"]
-    const [sortField, setSortField] = useState("name")
-    const [sortOrder, setSortOrder] = useState("asc")
-    const [pageNumber, setPageNumber] = useState(0)
-    const vendors = api.vendor.getVendors.useQuery(
-      {pageNumber:pageNumber, entriesPerPage:VENDORS_PER_PAGE, sortBy:sortField, descOrAsc:sortOrder}).data
-    const newVendor = api.vendor.createVendor.useMutation()
-    const numberOfPages = Math.ceil(api.vendor.getNumVendors.useQuery().data / VENDORS_PER_PAGE)
-    const numberOfEntries = api.vendor.getNumVendors.useQuery().data
-    const [displayEditVendorView, setDisplayEditVendorView] = useState(false)
-    const [displayDeleteVendorView, setDisplayDeleteVendorView] = useState(false)
-    const [displayVendorView, setDisplayVendorView] = useState(false)
-    const [currentVendor, setCurrentVendor] = useState({id: '', name: ''})
+  const VENDORS_PER_PAGE = 5
+  const FIRST_HEADER = ["Vendor Name", "name"]
+  const SORTABLE_HEADERS = []
+  const STATIC_HEADERS = ["Edit", "Delete"]
+  const [sortField, setSortField] = useState("name")
+  const [sortOrder, setSortOrder] = useState("asc")
+  const [pageNumber, setPageNumber] = useState(0)
+  const vendors = api.vendor.getVendors.useQuery(
+      {
+        pageNumber: pageNumber,
+        entriesPerPage: VENDORS_PER_PAGE,
+        sortBy: sortField,
+        descOrAsc: sortOrder
+      }).data
+  const newVendor = api.vendor.createVendor.useMutation()
+  const numberOfPages = Math.ceil(api.vendor.getNumVendors.useQuery().data / VENDORS_PER_PAGE)
+  const numberOfEntries = api.vendor.getNumVendors.useQuery().data
+  const [displayEditVendorView, setDisplayEditVendorView] = useState(false)
+  const [displayDeleteVendorView, setDisplayDeleteVendorView] = useState(false)
+  const [displayVendorView, setDisplayVendorView] = useState(false)
+  const [currentVendor, setCurrentVendor] = useState({id: '', name: ''})
 
-    const handleNewVendorSubmission = async (name: string) => {
-        if (newVendor){
-            newVendor.mutate({
-                name: name
-            })
-        }
-      }
-
-    async function openEditVendorView(id: string){
-      if (vendors){
-        for (const ven of vendors){
-          if (ven.id === id){
-            setCurrentVendor({
-              id: ven.id,
-              name: ven.name,
-            })
-          }
-        }
-        setDisplayEditVendorView(true)
-      }
+  const handleNewVendorSubmission = async (name: string) => {
+    if (newVendor) {
+      newVendor.mutate({
+        name: name
+      })
     }
-    function renderEditVendorView() {
-      return(
-          <>
-            {(displayEditVendorView && currentVendor) ?
-              <CreateEntries closeStateFunction={setDisplayEditVendorView} submitText="Edit Vendor">
-                <EditVendorModal closeOut={closeEditVendorView} vendorId={currentVendor.id} vendorName={currentVendor.name}></EditVendorModal>
-              </CreateEntries> : null}
-          </>
-      )
-    }
-    function closeEditVendorView(){
-      setDisplayEditVendorView(false)
-    }
-
-    async function openDeleteVendorView(id: string){
-      if (vendors){
-        for (const ven of vendors){
-          if (ven.id === id){
-            setCurrentVendor({
-              id: ven.id,
-              name: ven.name,
-            })
-          }
-        }
-        setDisplayDeleteVendorView(true)
-      }
-    }
-    function renderDeleteVendorView() {
-      return <>
-        {(displayDeleteVendorView && currentVendor) ?
-            <CreateEntries closeStateFunction={setDisplayDeleteVendorView} submitText="Delete Vendor">
-              <DeleteVendorModal closeOut={closeDeleteVendorView} vendorId={currentVendor.id} vendorName={currentVendor.name}></DeleteVendorModal></CreateEntries> : null}
-      </>;
-    }
-    function closeDeleteVendorView(){
-      setDisplayDeleteVendorView(false)
-    }
-
-    async function openVendorView(id: string){
-      if (vendors){
-        for (const ven of vendors){
-          if (ven.id === id){
-            setCurrentVendor({
-              id: ven.id,
-              name: ven.name,
-            })
-          }
-        }
-        setDisplayVendorView(true)
-      }
-    }
-    function renderVendorView() {
-      return <>
-        {(displayVendorView && currentVendor) ?
-            <CreateEntries closeStateFunction={setDisplayVendorView} submitText="Edit Vendor">
-              <ViewVendorModal closeOut={closeVendorView} vendorId={currentVendor.id} vendorName={currentVendor.name} openEdit={openEditVendorView}></ViewVendorModal></CreateEntries> : null}
-      </>;
-    }
-    function closeVendorView(){
-      setDisplayVendorView(false)
-    }
-
-  function renderVendorRow(){
-    return(vendors ? vendors.map((vendor)=>(<VendorTableRow onView={openVendorView} onEdit={openEditVendorView} onDelete={openDeleteVendorView} vendorInfo={{id: vendor.id, name: vendor.name}} ></VendorTableRow>)): null)
   }
 
+  async function openEditVendorView(id: string) {
+    if (vendors) {
+      for (const ven of vendors) {
+        if (ven.id === id) {
+          setCurrentVendor({
+            id: ven.id,
+            name: ven.name,
+          })
+        }
+      }
+      setDisplayEditVendorView(true)
+    }
+  }
+
+  function renderEditVendorView() {
+    return (
+        <>
+          {(displayEditVendorView && currentVendor) ?
+              <CreateEntries closeStateFunction={setDisplayEditVendorView} submitText="Edit Vendor">
+                <EditVendorModal closeOut={closeEditVendorView} vendorId={currentVendor.id}
+                                 vendorName={currentVendor.name}></EditVendorModal>
+              </CreateEntries> : null}
+        </>
+    )
+  }
+
+  function closeEditVendorView() {
+    setDisplayEditVendorView(false)
+  }
+
+  async function openDeleteVendorView(id: string) {
+    if (vendors) {
+      for (const ven of vendors) {
+        if (ven.id === id) {
+          setCurrentVendor({
+            id: ven.id,
+            name: ven.name,
+          })
+        }
+      }
+      setDisplayDeleteVendorView(true)
+    }
+  }
+
+  function renderDeleteVendorView() {
+    return <>
+      {(displayDeleteVendorView && currentVendor) ?
+          <CreateEntries closeStateFunction={setDisplayDeleteVendorView} submitText="Delete Vendor">
+            <DeleteVendorModal closeOut={closeDeleteVendorView} vendorId={currentVendor.id}
+                               vendorName={currentVendor.name}></DeleteVendorModal></CreateEntries> : null}
+    </>;
+  }
+
+  function closeDeleteVendorView() {
+    setDisplayDeleteVendorView(false)
+  }
+
+  async function openVendorView(id: string) {
+    if (vendors) {
+      for (const ven of vendors) {
+        if (ven.id === id) {
+          setCurrentVendor({
+            id: ven.id,
+            name: ven.name,
+          })
+        }
+      }
+      setDisplayVendorView(true)
+    }
+  }
+
+  function renderVendorView() {
+    return <>
+      {(displayVendorView && currentVendor) ?
+          <CreateEntries closeStateFunction={setDisplayVendorView} submitText="Edit Vendor">
+            <ViewVendorModal closeOut={closeVendorView} vendorId={currentVendor.id}
+                             vendorName={currentVendor.name}
+                             openEdit={openEditVendorView}></ViewVendorModal></CreateEntries> : null}
+    </>;
+  }
+
+  function closeVendorView() {
+    setDisplayVendorView(false)
+  }
+
+  function renderVendorRow() {
+    return (vendors ? vendors.map((vendor) => (
+        <VendorTableRow onView={openVendorView} onEdit={openEditVendorView}
+                        onDelete={openDeleteVendorView}
+                        vendorInfo={{id: vendor.id, name: vendor.name}}></VendorTableRow>)) : null)
+  }
 
   return (
       <div className="px-4 sm:px-6 lg:px-8">
         <TableDetails tableName="Vendors" tableDescription="A list of all the Vendors.">
-          <AddVendorModal showVendorEdit={handleNewVendorSubmission} buttonText="Add New Vendor" submitText="Add Vendor"></AddVendorModal>
+          <AddVendorModal showVendorEdit={handleNewVendorSubmission} buttonText="Add New Vendor"
+                          submitText="Add Vendor"></AddVendorModal>
         </TableDetails>
-        <Table sorting = {{setOrder:setSortOrder, setField:setSortField, currentOrder:sortOrder, currentField:sortField}}
-               setPage= {setPageNumber}
+        <Table sorting={{
+          setOrder: setSortOrder,
+          setField: setSortField,
+          currentOrder: sortOrder,
+          currentField: sortField
+        }}
+               setPage={setPageNumber}
                firstHeader={FIRST_HEADER}
                sortableHeaders={SORTABLE_HEADERS}
                staticHeaders={STATIC_HEADERS}
-               items= {vendors}
+               items={vendors}
                headersNotFiltered={["price", "inventory"]}
                pageNumber={pageNumber}
                numberOfPages={numberOfPages}
@@ -146,5 +167,4 @@ export default function VendorTable() {
         </div>
       </div>
   )
-
 }

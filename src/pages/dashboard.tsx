71b0ca3a77138c4e89c1b8c5084b58af5f9389c1
AsherEarly <asher.early@duke.edu>
2023-02-27 14:51:37 -0500

    removed test code on dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -9,20 +9,6 @@ export default function DashboardPage() {
     return (
         <>
          <AppShell activePage="Dashboard"></AppShell>
-          <CldUploadButton
-              uploadPreset="book-image-preset"
-              className="inline-flex w-1/3 justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
-              onUpload={(result, widget) => {
-                console.log(result.info.public_id)
-              }}
-
-          />
-          <CldImage
-              width="600"
-              height="600"
-              src="https://res.cloudinary.com/dyyevpzdz/image/upload/v1677264732/book-covers/lisphiz2ltw9oew0urvp.png"
-              alt="My Image"
-          />
         </>
     )
 }

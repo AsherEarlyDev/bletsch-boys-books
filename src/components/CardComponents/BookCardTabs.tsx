c40c0328bf4a2e02d4811cadc78d274195d46738
Robert Cranston <rvc6@duke.edu>
2023-03-20 18:21:20 -0400

    fix error with case

--- src/components/CardComponents/BookCardTabs.tsx
+++ src/components/CardComponents/BookCardTabs.tsx
@@ -16,7 +16,7 @@ export default function BookCardTabs(props: BookCardTabsProps) {
   const transactionDetails = api.books.getBookTransactionDetails.useQuery(props.isbn).data
   const relatedBooks = api.books.findRelatedBooks.useQuery({isbn:props.isbn, title:props.title}).data
   let [categories] = useState(transactionDetails)
-
+  console.log(transactionDetails)
   return (
       <Tab.Group>
         <Tab.List className="flex space-x-1 rounded-xl bg-indigo-300 p-1 mx-5">
@@ -58,14 +58,14 @@ export default function BookCardTabs(props: BookCardTabsProps) {
                 }
                   <ul>
                   {transactionDetails.map((transaction) => (
-                      <Link href={{pathname: (transaction.type==="Sale" ? '/sales' : (transaction.type==="Purchase" ? "/purchases" : "/buybacks")), query:{openView:"true", viewId:(transaction.type==="Sale" ? transaction.saleReconciliation.id : (transaction.type==="Purchase" ? transaction.purchaseOrder.id : transaction.bookBuybackOrder.id))}}}><li
+                      <Link href={{pathname: (transaction.type==="Sale" ? '/sales' : (transaction.type==="Purchase" ? "/purchases" : "/buybacks")), query:{openView:"true", viewId:(transaction.type==="Sale" ? transaction.saleReconciliation.id : (transaction.type==="Purchase" ? transaction.purchaseOrder.id : transaction.BookBuybackOrder.id))}}}><li
                           key={transaction.id}
                           className="relative rounded-md p-3 hover:bg-indigo-100 flex flex-row justify-between text-left"
                       >
                         <h3 className="text-sm font-medium leading-5 w-32">{transaction.type}</h3>
                         <h3 className="text-sm font-medium leading-5 w-32">{transaction.date}</h3>
                         <h3 className="text-sm font-medium leading-5 w-32">Add User Details</h3>
-                        <h3 className="text-sm font-medium leading-5 w-32">{transaction.purchaseOrder?.vendor.name ?? transaction.bookBuybackOrder?.vendor.name ?? ""}</h3>
+                        <h3 className="text-sm font-medium leading-5 w-32">{transaction.purchaseOrder?.vendor.name ?? transaction.BookBuybackOrder?.vendor.name ?? ""}</h3>
                         <h3 className="text-sm font-medium leading-5 w-32">{"$"+(transaction.price?.toFixed(2) ?? transaction.buybackPrice?.toFixed(2) ?? "0.00")}</h3>
                         <h3 className="text-sm font-medium leading-5 w-32">{transaction.quantity}</h3>
                         <h3 className="text-sm font-medium leading-5 w-32">{transaction.inventory}</h3>

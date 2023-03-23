6031fb27fe75554c1a1193899fc179ed56e44de1
Robert Cranston <rvc6@duke.edu>
2023-03-22 21:05:45 -0500

    next

--- src/components/TableComponents/Modals/OrganizerModals/ShelfCalculatorModal.tsx
+++ src/components/TableComponents/Modals/OrganizerModals/ShelfCalculatorModal.tsx
@@ -4,6 +4,13 @@ import { toast } from "react-toastify";
 import MutableCardProp from '../../../CardComponents/MutableCardProp';
 import { ReactSortable } from "react-sortablejs";
 import BookCardProp from '../../../CardComponents/BookCardProp';
+import { api } from '../../../../utils/api';
+import TableDetails from '../../TableDetails';
+import TableHeader from '../../TableHeader';
+import ColumnHeading from "../../TableColumnHeadings/ColumnHeading"
+;
+import TableEntry from '../../TableEntries/TableEntry';
+import MutableTableEntry from '../../TableEntries/MutableTableEntry';
 interface ShelfCalculatorModalProp{
   buttonText: string;
   submitText: string;
@@ -17,8 +24,10 @@ export default function ShelfCalculatorModal(props: ShelfCalculatorModalProp) {
   const DEFAULT_WIDTH = 20
   const [isOpen, setIsOpen] = useState(false)
   const [width, setWidth] = useState(DEFAULT_WIDTH)
-  const [bookList, setBookList] = useState([{name:"a noble radience"}, {name:"a noble radience"}])
-  const [currentBook, setCurrentBook] = useState("")
+  const [bookList, setBookList] = useState([])
+  const [currentBookISBN, setCurrentBook] = useState("")
+  const currentBookObj = api.books.findBooks.useQuery([currentBookISBN]).data
+  const currentBook = currentBookObj?.internalBooks[0] ?? {}
 
   function closeModal() {
     setIsOpen(false)
@@ -91,33 +100,70 @@ export default function ShelfCalculatorModal(props: ShelfCalculatorModalProp) {
                     leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
-                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                    <form method="post" onSubmit={handleSubmit}>
-                      <div>
-                        <div className="text-center">
-                          <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                            Shelf Calculator
-                          </Dialog.Title>
-                          <p className="font-small leading-6 text-gray-900">
-                            Set shelf width first
-                          </p>
+                  <Dialog.Panel className="relative transform overflow-y-auto rounded-lg bg-white px-4 pt-5 pb-4 shadow-xl transition-all sm:my-8  ">
+                      <div>                     
+                        <div className="px-4 sm:px-6 lg:px-8 rounded-lg shadow-lg py-8 bg-white">
+                          <div className="mb-8">
+                            <TableDetails tableName={"Shelf Calculator"} tableDescription={"Calculate available shelf space"}>
+                            </TableDetails>
+                            <div className="mt-5">
+                              <MutableCardProp saveValue={setWidth} heading="Width" dataType="number" defaultValue={DEFAULT_WIDTH}></MutableCardProp>
+                              <BookCardProp saveFunction={setCurrentBook} ></BookCardProp>
+                              <div><button type="button"
+                                  onClick={() => {
+                                    if(currentBook.title){
+                                      setBookList([...bookList, { name:currentBook.title, inventory: currentBook.inventory, displayCount:0, dimensions: currentBook.dimensions}])
+                                    }
+
+                                }}
+                                  className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                              >Add book</button></div>
+                            </div>
+                            <div className="mt-8 flex flex-col">
+                              <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                                  <div className="overflow-auto shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                    <table className="min-w-full divide-y divide-gray-300 table-auto">
+                                      <TableHeader>
+                                        <ColumnHeading firstEntry={true} label="Title"></ColumnHeading>
+                                        <ColumnHeading label="Inventory"></ColumnHeading>
+                                        <ColumnHeading label="Display Count"></ColumnHeading>
+                                        <ColumnHeading label="Shelf Space"></ColumnHeading>
+                                        <ColumnHeading label="Delete"></ColumnHeading>
+                                      </TableHeader>
+                                        <ReactSortable
+                                          filter=".addImageButtonContainer"
+                                          className="divide-y divide-gray-200 bg-white"
+                                          tag="tbody"
+                                          dragClass="sortableDrag"
+                                          list={bookList}
+                                          setList={setBookList}
+                                          animation={200}
+                                          easing="ease-out">
+                                          {bookList.map((item, idx) => (
+                                            <tr className="draggableItem">
+                                              <TableEntry firstEntry={true}>{item.name}</TableEntry>
+                                              <TableEntry>{item.inventory}</TableEntry>
+                                              <MutableTableEntry saveValue={(displayCount) =>{
+                                                const temp = [...bookList]
+                                                temp[idx] = {...item, displayCount:displayCount}
+                                                setBookList(temp)
+                                              } } heading="Display Count"
+                                                 required="True" dataType="number"
+                                                 defaultValue={0}></MutableTableEntry>
+                                              {/* Dimensions are width thickness then height */}
+                                              <TableEntry>{(item.displayCount * (item.dimensions[1] ? item.dimensions[1] : 0.8)).toFixed(2)}"</TableEntry>
+                                              <TableEntry firstEntry={true}>{item.dimensions}</TableEntry>
+                                            </tr>
+                                          ))}
+                                        </ReactSortable>
+                                    </table>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                        <div className="mt-5">
-                        <MutableCardProp saveValue={setWidth} heading="Width" dataType="number" defaultValue={DEFAULT_WIDTH}></MutableCardProp>
-                        <BookCardProp saveFunction={setCurrentBook} ></BookCardProp>
-                        </div>
-                        <ReactSortable
-                          filter=".addImageButtonContainer"
-                          dragClass="sortableDrag"
-                          list={bookList}
-                          setList={setBookList}
-                          animation={200}
-                          easing="ease-out"
-                        >
-                          {bookList.map(item => (
-                            <div className="draggableItem">{item.name}</div>
-                          ))}
-                        </ReactSortable>
                       </div>
                       <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                         <button
@@ -134,7 +180,6 @@ export default function ShelfCalculatorModal(props: ShelfCalculatorModalProp) {
                           Cancel
                         </button>
                       </div>
-                    </form>
                   </Dialog.Panel>
                 </Transition.Child>
               </div>

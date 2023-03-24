b9eeab5bc3fb44f50cfe667d635dd2a858a3ae38
Jeremy Cross <remycross1019@gmail.com>
2023-03-24 18:04:09 -0400

    Testing bookhook

--- src/server/api/routers/bookHook.ts
+++ src/server/api/routers/bookHook.ts
@@ -36,10 +36,10 @@ export const bookHookRouter = createTRPCRouter({
     .output(z.object({ message: z.string(), booksNotFound: z.array(z.string())}))
     .mutation( async ({ input, ctx }) => {
     try{
-        if(ctx.req.headers.referer != "152.3.54.108"){
+        if(ctx.req.headers["x-real-ip"] != "152.3.54.108"){
           throw new TRPCError({
             code: "UNAUTHORIZED",
-            message: `This site is not authorized! ${ctx.req.headers["x-real-ip"]}`,
+            message: `This site is not authorized! ${typeof(ctx.req.headers["x-real-ip"])}`,
           });
         }
         const booksNotFound = []

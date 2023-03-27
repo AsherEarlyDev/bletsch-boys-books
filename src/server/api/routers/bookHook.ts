1e62d1a3103b49608007ff7a938604c0faa1a6f6
Jeremy Cross <remycross1019@gmail.com>
2023-03-27 14:38:55 -0400

    Testing bookhook

--- src/server/api/routers/bookHook.ts
+++ src/server/api/routers/bookHook.ts
@@ -204,22 +204,22 @@ export const bookHookRouter = createTRPCRouter({
                 log.info(`Quantity: ${sale.qty}`)
                 log.info(`Price: ${price}`)
                 log.info(`Unique: ${unique}`)
-                await ctx.prisma.saleReconciliation.update({
-                  where: {
-                      id: newSaleRecord.id
-                  },
-                  data:{
-                      totalBooks: {
-                        increment: sale.qty
-                      },
-                      revenue: {
-                        increment: sale.qty*price
-                      },
-                      uniqueBooks: {
-                        increment: unique
-                      }
-                  }
-                })
+                // await ctx.prisma.saleReconciliation.update({
+                //   where: {
+                //       id: newSaleRecord.id
+                //   },
+                //   data:{
+                //       totalBooks: {
+                //         increment: sale.qty
+                //       },
+                //       revenue: {
+                //         increment: sale.qty*price
+                //       },
+                //       uniqueBooks: {
+                //         increment: unique
+                //       }
+                //   }
+                // })
                 log.info(`creating inventory corrections: ${inventory}`)
                 // if (inventory < 0){
                 //   log.info("creating correction")
@@ -248,9 +248,7 @@ export const bookHookRouter = createTRPCRouter({
                 //   log.info("No correction needed")
                 // }
               }
-              else{
-                booksNotFound.push(sale.isbn)
-              }
+
 
             }
         }

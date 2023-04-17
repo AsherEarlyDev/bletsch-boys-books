31b15b090c3142ca1f0e14ee8c16a066ea264d22
Jeremy Cross <remycross1019@gmail.com>
2023-04-16 22:47:41 -0400

    trying stuff

--- src/components/TableComponents/Modals/UserModals/AddUserModal.tsx
+++ src/components/TableComponents/Modals/UserModals/AddUserModal.tsx
@@ -2,8 +2,6 @@ import { Dialog, Transition } from '@headlessui/react'
 import React, {Fragment, useRef, useState} from 'react'
 import {api} from "../../../../utils/api";
 import {toast} from "react-toastify";
-import { concat } from 'cypress/types/lodash';
-import { redirect } from 'next/navigation';
 
 interface UserModalProp{
   buttonText: string;
@@ -25,7 +23,7 @@ export default function AddUserModal(props: UserModalProp) {
   });
   function handleCreateNewUser(){
     if (password === confirmPassword){
-      const create = createUser.mutate({
+      createUser.mutate({
         password: password,
         username: username,
         role: (isAdmin ? "ADMIN" : "USER")
@@ -81,7 +79,7 @@ export default function AddUserModal(props: UserModalProp) {
                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
                   <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                    <form className="space-y-6" action="#" method="post" onSubmit={handleCreateNewUser}>
+                    <form className="space-y-6" method="post" onSubmit={handleCreateNewUser}>
 
                             <div>
                               <label htmlFor="username" className="block text-sm font-medium text-gray-700">

a7f4dba9f438fbed50d656b79c331e069dcba8b1
Jeremy Cross <remycross1019@gmail.com>
2023-04-16 22:52:00 -0400

    trying stuff

--- src/components/TableComponents/Modals/UserModals/AddUserModal.tsx
+++ src/components/TableComponents/Modals/UserModals/AddUserModal.tsx
@@ -79,7 +79,6 @@ export default function AddUserModal(props: UserModalProp) {
                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
                   <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                    <form className="space-y-6" method="post" onSubmit={handleCreateNewUser}>
 
                             <div>
                               <label htmlFor="username" className="block text-sm font-medium text-gray-700">
@@ -146,11 +145,11 @@ export default function AddUserModal(props: UserModalProp) {
                               <button
                                   type="submit"
                                   className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                  onClick={handleCreateNewUser}
                               >
                                 Create New User
                               </button>
                             </div>
-                          </form>
                   </Dialog.Panel>
                 </Transition.Child>
               </div>

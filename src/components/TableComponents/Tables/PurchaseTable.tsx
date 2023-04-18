89ac9076d0dcc2c4d01851b383454ba201ee9f51
robertcranston-3 <59669734+robertcranston-3@users.noreply.github.com>
2023-04-18 02:50:19 -0400

    Last (#136)
    * fix organizer taken space
    
    * switch around bookcase display
    
    * fix buyback
    
    * fix buyback
    
    ---------
    
    Co-authored-by: robertcranston-3 <robertcranston@Roberts-MBP.lan>
    Co-authored-by: AsherEarlyDev <122934131+AsherEarlyDev@users.noreply.github.com>

--- src/components/TableComponents/Tables/PurchaseTable.tsx
+++ src/components/TableComponents/Tables/PurchaseTable.tsx
@@ -51,6 +51,7 @@ export default function PurchaseTable() {
   const viewCurrentPurchases = viewCurrentOrder ? viewCurrentOrder.purchases : undefined
   const createPurchaseOrder = api.purchaseOrder.createPurchaseOrder.useMutation({
     onSuccess: ()=>{
+      setOnlyEdit(false)
       setDisplayEditPurchaseView(true)
     }
   })
